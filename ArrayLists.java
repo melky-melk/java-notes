cee43a6a14570ee5e1fde50edadf095f80020d7d
melky-melk <84921717+melky-melk@users.noreply.github.com>
2021-09-24 13:12:17 +1000

    Create ArrayLists.java

---
+++ ArrayLists.java
@@ -0,0 +1,22 @@
+public class ArrayLists {
+	public static main(String[] args){
+		//ArrayList = a resizable array. Elements can be added and removed after the comilation phase, only store reference data types
+		//if you wanted arraylist of integers it would not be ArrayList<int> rather, ArrayList<Integer> 
+		//because int is a primitive type whereas Integer is a reference type
+		//The typical syntax for an arrat list goes"
+		//ArrayList<Class> listName = new ArrayList<Class>();
+		ArrayList<String> food = new ArrayList<String>();
+
+		//to add things to an array list:
+		food.add("pizza");//
+		food.add("hamburger");
+		food.add("hotdog");
+
+		//and you can loop through array lists like any other array 
+		// to access items with it you use the word
+		food.get(1)// to get index 1 
+
+		//to get the length
+		food.size() //how many items are in the arraylist
+	}
+}
