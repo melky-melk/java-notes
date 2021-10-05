17a3d69472e6eb38765362405af40ed85068c0c1
melky-melk <84921717+melky-melk@users.noreply.github.com>
2021-10-05 17:28:23 +1100

    quiz 1 answers added

---
+++ Quiz1/ParentClass.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class ParentClass {
+	private String name;
+
+	public ParentClass(String name){
+		this.name = name;
+	}
+
+	public void setName(String name) {this.name = name;}
+	public String getName()	{return this.name;}
+}
+
+//subclass of parent class, inherits private name
+class Applicant extends ParentClass {
+	public String[] applicantSkills;
+
+	public Applicant(String name, String[] applicantSkills){
+		// uses the super's constuctor to create the object with the name
+		super(name);
+		// then assigns the array to the subclass's attribute
+		this.applicantSkills = applicantSkills;
+	}
+}
+
+class Employer extends ParentClass {
+	private String[] desiredSkills;
+
+	public Employer(String name, String[] desiredSkills){
+		// uses the super's constuctor to create the object with the name
+		super(name);
+		// then assigns the array to the subclass's attribute
+		this.desiredSkills = desiredSkills;
+	} 
+
+	public List<String> ShortlistedApplicants(List<Applicant> listOfApplicants) {
+		ArrayList<String> namesOfApplicants = new ArrayList<String>();
+
+		//goes through the applicant objects
+		for (Applicant applicant : listOfApplicants){
+			
+			//then gets the skill array from the applicant and gets the string and goes through every single skill
+			for (String applicantSkill : applicant.applicantSkills) {
+				
+				//then checks all of desired skills within the employer's array to see for a match
+				for (String desiredSkill : desiredSkills) {
+					
+					// then checks if the applicant's skill matches
+					if (applicantSkill.toUpperCase().equals(desiredSkill.toUpperCase())){
+
+						//then adds it to the applicants that have teh skill
+						namesOfApplicants.add(applicant.getName());
+					}
+				}
+			}
+		}
+		//after the loop is completed the duplicate applicants have to be removed
+		//then makes a new set from the array list to remove all duplicates, linked hashset makes sure its still the same order
+		Set<String> set = new LinkedHashSet<>(namesOfApplicants);
+
+		//removes all items from the original set
+		namesOfApplicants.clear();
+		// then adds all the set back into the list all without duplicates
+		namesOfApplicants.addAll(set);
+		
+		return namesOfApplicants;
+	}
+}
---
+++ Quiz1/ReadEmployees.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+
+public class ReadEmployees {
+	public static void main(String[] args){
+		//gets the filepath
+		String filename = args[0];
+       
+        try{
+			// creates a new hashmap for the company name, with an arraylist for all the employee names inside the company
+            HashMap<String, ArrayList<String>> allCompaniesWithEmployees = new HashMap<String, ArrayList<String>>();
+
+			File f = new File(filename);
+            Scanner sc = new Scanner(f);
+
+            while (sc.hasNextLine()){
+				//initiallises the array lists
+				ArrayList<String> newEmployeeList = new ArrayList<String>();
+				ArrayList<String> existingEmployeeList;
+
+                String[] lineArray = (sc.nextLine()).split(" ");
+
+                String companyName = lineArray[0];
+                String nameOfEmployee = lineArray[1];
+
+				// if the hashmap already contains the company's name then it adds 
+				if (allCompaniesWithEmployees.containsKey(companyName)) {
+					//gets the employee list that already exists, adds the new employee to the existing list then sets the company with the new list
+					existingEmployeeList = allCompaniesWithEmployees.get(companyName);
+					existingEmployeeList.add(nameOfEmployee);
+					allCompaniesWithEmployees.put(companyName, existingEmployeeList);
+				} else {
+					// adds a single employee so it can be put into the array list
+					newEmployeeList.add(nameOfEmployee);
+					allCompaniesWithEmployees.put(companyName, newEmployeeList);
+				}
+            }
+
+			// initialises values
+			//sets the largest employee as the lowest possible
+			int mostEmployees = -1;
+			String largestCompanyName = "";
+			ArrayList<String> employeeList;
+			
+			// gets all the company's names as a set then goes through all the company's names
+			for (String companyName : allCompaniesWithEmployees.keySet()) {
+				//gets the employee list from the key of the company
+				employeeList = allCompaniesWithEmployees.get(companyName);
+
+				// checks if its larger
+				if (employeeList.size() > mostEmployees){
+					mostEmployees = employeeList.size();
+					largestCompanyName = companyName;
+				} 
+			}
+
+			System.out.println(largestCompanyName);
+			for (String name : (allCompaniesWithEmployees.get(largestCompanyName))) {
+				System.out.println(name);
+			}
+
+        } catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
